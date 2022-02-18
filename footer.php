615ad4aa1a0e1237808958a92074c9f9288c57dd
BadbooX <k.cotton.pro@gmail.com>
2022-02-18 15:32:56 +0100

    Commit kéké/hassan

---
+++ footer.php
@@ -0,0 +1,16 @@
+<footer class="fitou">
+        <div class="blabla"><p>Copyright © 2018 Chen, Yi-Ya.</p></div>
+        <div class="allico">
+            <div class="ico">
+                <i id="bax" class="fa-brands fa-facebook-f"></i>
+            </div>
+            <div class="ico">
+                <i id="bbx" class="fa-brands fa-twitter"></i>
+            </div>
+            <div class="ico"><i id="bcx" class="fa fa-medium"></i>
+            </div>
+            <div class="ico">
+                <i id="bdx" class="fa-brands fa-linkedin-in"></i>
+            </div>
+        </div>
+</footer>
